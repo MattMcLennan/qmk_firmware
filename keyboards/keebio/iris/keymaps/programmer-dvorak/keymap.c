1ae1b6910255b493b6f5fd91ca3631b1e52e650c
MattMcLennan <mclennan6891@gmail.com>
2022-12-24 00:41:28 -0500

    refactor: remove use of autoshift for override functionality

--- keyboards/keebio/iris/keymaps/programmer-dvorak/keymap.c
+++ keyboards/keebio/iris/keymaps/programmer-dvorak/keymap.c
@@ -17,36 +17,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_PIPE, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_BSLS, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    _______,          _______,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,   KC_AT,
+     KC_LSFT, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    _______,          _______,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,   KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LCTL,  KC_ENT,   FUNC,                    KC_ESC,  KC_SPC,  KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  // [_AUTO_SHIFTED] = LAYOUT(
-  // //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-  //    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     _______,         all non-standard
-  // //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-  //    KC_TAB,  KC_COLN, KC_LT,    KC_GT,  KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_QUES,
-  // //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-  //    KC_EXLM, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_UNDS,          exlm, non-standard
-  // //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-  //    KC_PERC, KC_DQT,  KC_Q,    KC_J,    KC_K,    KC_X,    _______,          _______,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,   KC_HASH,          perc, hash non-standard
-  // //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-  //                                   _______,  _______, _______,                  _______,  _______, _______
-  //                               // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-  // ),
-
-
   [_FUNC] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                               KC_F6,  KC_F7,    KC_F8,   KC_F9,  KC_F10,  KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______,  _______, _______, KC_HOME,                           KC_CIRC, KC_F11,  KC_F12, _______, KC_RIGHT, KC_PAGE_UP,
+     _______, _______, _______,  _______, _______, KC_HOME,                            KC_F11,  KC_F12,  KC_UP, _______, _______, KC_PAGE_UP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______,  RESET,   _______,  _______, _______, KC_END,                           _______,  KC_LEFT, _______,  _______, _______, KC_PGDN,
+     _______,  RESET,   _______,  _______, _______, KC_END,                           KC_CIRC, KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_PGDN,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, _______, _______,  KC_DOWN, KC_UP,  _______, _______,       _______, _______, KC_LEFT_GUI, _______,  _______, _______, KC_RSFT,
+     KC_LSFT, KC_BSLS, _______,  _______, _______,  _______, _______,       _______, _______, KC_LEFT_GUI, _______,  _______, KC_AT, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______,  _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -54,130 +39,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
-    switch(keycode) {
-        case KC_DLR:
-        case KC_PLUS:
-        case KC_LBRC:
-        case KC_LCBR:
-        case KC_LPRN:
-        case KC_AMPR:
-        case KC_EQL:
-        case KC_RPRN:
-        case KC_RCBR:
-        case KC_RBRC:
-        case KC_ASTR:
-        case KC_PIPE:
-        case KC_BSLS:
-        case KC_AT:
-            return true;
-        default:
-            return false;
-    }
-}
+const key_override_t dollar_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DLR, KC_TILD);
+const key_override_t plus_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_PLUS, KC_1);
+const key_override_t leftbracket_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, KC_2);
+const key_override_t leftcurlybrace_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LCBR, KC_3);
+const key_override_t leftparenthesis_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_4);
+const key_override_t ampersand_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_AMPR, KC_5);
+const key_override_t equals_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_EQL, KC_6);
+const key_override_t rightparenthesis_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_7);
+const key_override_t rightcurlybrace_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RCBR, KC_8);
+const key_override_t rightbracket_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RBRC, KC_9);
+const key_override_t asterick_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_ASTR, KC_0);
+const key_override_t pipe_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_PIPE, KC_EXLM);
+const key_override_t backslash_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSLS, KC_PERC);
+const key_override_t at_shifted_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_AT, KC_HASH);
 
-void autoshift_press_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
-    switch(keycode) {
-        case KC_DLR:
-            register_code16((!shifted) ? KC_DLR : KC_TILD);
-            break;
-        case KC_PLUS:
-            register_code16((!shifted) ? KC_PLUS : KC_1);
-            break;
-        case KC_LBRC:
-            register_code16((!shifted) ? KC_LBRC : KC_2);
-            break;
-        case KC_LCBR:
-            register_code16((!shifted) ? KC_LCBR : KC_3);
-            break;
-        case KC_LPRN:
-            register_code16((!shifted) ? KC_LPRN : KC_4);
-            break;
-        case KC_AMPR:
-            register_code16((!shifted) ? KC_AMPR : KC_5);
-            break;
-        case KC_EQL:
-            register_code16((!shifted) ? KC_EQL : KC_6);
-            break;
-        case KC_RPRN:
-            register_code16((!shifted) ? KC_RPRN : KC_7);
-            break;
-        case KC_RCBR:
-            register_code16((!shifted) ? KC_RCBR : KC_8);
-            break;
-        case KC_RBRC:
-            register_code16((!shifted) ? KC_RBRC : KC_9);
-            break;
-        case KC_ASTR:
-            register_code16((!shifted) ? KC_ASTR : KC_0);
-            break;
-        case KC_PIPE:
-            register_code16((!shifted) ? KC_PIPE : KC_EXLM);
-            break;
-        case KC_BSLS:
-            register_code16((!shifted) ? KC_BSLS : KC_PERC);
-            break;
-        case KC_AT:
-            register_code16((!shifted) ? KC_AT : KC_HASH);
-            break;
-       default:
-            if (shifted) {
-                add_weak_mods(MOD_BIT(KC_LSFT));
-            }
-        // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
-            register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
-    }
-}
-
-void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
-    switch(keycode) {
-        case KC_DLR:
-            unregister_code16((!shifted) ? KC_DLR : KC_TILD);
-            break;
-        case KC_PLUS:
-            unregister_code16((!shifted) ? KC_PLUS : KC_1);
-            break;
-        case KC_LBRC:
-            unregister_code16((!shifted) ? KC_LBRC : KC_2);
-            break;
-        case KC_LCBR:
-            unregister_code16((!shifted) ? KC_LCBR : KC_3);
-            break;
-        case KC_LPRN:
-            unregister_code16((!shifted) ? KC_LPRN : KC_4);
-            break;
-        case KC_AMPR:
-            unregister_code16((!shifted) ? KC_AMPR : KC_5);
-            break;
-        case KC_EQL:
-            unregister_code16((!shifted) ? KC_EQL : KC_6);
-            break;
-        case KC_RPRN:
-            unregister_code16((!shifted) ? KC_RPRN : KC_7);
-            break;
-        case KC_RCBR:
-            unregister_code16((!shifted) ? KC_RCBR : KC_8);
-            break;
-        case KC_RBRC:
-            unregister_code16((!shifted) ? KC_RBRC : KC_9);
-            break;
-        case KC_ASTR:
-            unregister_code16((!shifted) ? KC_ASTR : KC_0);
-            break;
-        case KC_PIPE:
-            unregister_code16((!shifted) ? KC_PIPE : KC_EXLM);
-            break;
-        case KC_BSLS:
-            unregister_code16((!shifted) ? KC_BSLS : KC_PERC);
-            break;
-        case KC_AT:
-            unregister_code16((!shifted) ? KC_AT : KC_HASH);
-            break;
-         default:
-            // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
-            // The IS_RETRO check isn't really necessary here, always using
-            // keycode & 0xFF would be fine.
-            unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
-    }
-}
-
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &dollar_shifted_key_override,
+    &plus_shifted_key_override,
+    &leftbracket_shifted_key_override,
+    &leftcurlybrace_shifted_key_override,
+    &leftparenthesis_shifted_key_override,
+    &ampersand_shifted_key_override,
+    &equals_shifted_key_override,
+    &rightparenthesis_shifted_key_override,
+    &rightcurlybrace_shifted_key_override,
+    &rightbracket_shifted_key_override,
+    &asterick_shifted_key_override,
+    &pipe_shifted_key_override,
+    &backslash_shifted_key_override,
+    &at_shifted_key_override,
+    NULL // Null terminate the array of overrides!
+};
